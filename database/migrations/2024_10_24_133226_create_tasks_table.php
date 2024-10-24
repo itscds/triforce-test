7c72fd4538d88e57bd2cfd39dbf25e82d231d442
itscds <chandrapalsolanki18@gmail.com>
2024-10-24 19:17:33 +0530

    Added Task model and migration

--- database/migrations/2024_10_24_133226_create_tasks_table.php
+++ database/migrations/2024_10_24_133226_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('task_name');
+            $table->boolean('is_completed')->default(false);
+            $table->dateTime('due_date');
             $table->timestamps();
         });
     }
